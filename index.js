1ddfc8325ac16ef2d8c6d4f23bec05dc1494b19e
Bryce Schmisseur <b.schmisseur@gmail.com>
2024-08-01 21:47:37 -0700

    Inital commit of files creating node js project structure

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @file index.js
+ * 
+ * @brief This file contains functionality to be used as the main entry point of 
+ * this JavaScript application
+ * 
+ * @author Bryce Schmisseur
+ */
+
+console.log("Hello World!")
+
